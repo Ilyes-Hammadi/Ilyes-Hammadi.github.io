2255a6cce9d2154284cc105387f39e709f38f132
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-03-20 20:16:42 +0100

    update nav bar menu

--- js/scripts.js
+++ js/scripts.js
@@ -10,76 +10,186 @@
     portfolio website.
 */
 
-(function($) {
+(function ($) {
 
-    // Remove no-js class
-    $('html').removeClass('no-js');
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+	// Remove no-js class
+	$('html').removeClass('no-js');
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+	// Animate to section when nav is clicked
+	$('header a').click(function (e) {
+		e.preventDefault();
+		var heading = $(this).attr('href');
+		var scrollDistance = $(heading).offset().top;
 
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
-    });
+		$('html, body').animate({
+			scrollTop: scrollDistance + 'px'
+		}, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
 
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
+		// Hide the menu once clicked if mobile
+		if ($('header').hasClass('active')) {
+			$('header, body').removeClass('active');
+		}
+	});
 
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
+	// Scroll to top
+	$('#to-top').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+	});
 
-    // Create timeline
-    $('#experience-timeline').each(function() {
+	// Scroll to first element
+	$('#lead-down span').click(function () {
+		var scrollDistance = $('#lead').next().offset().top;
+		$('html, body').animate({
+			scrollTop: scrollDistance + 'px'
+		}, 500);
+	});
 
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
+	// Create timeline
+	$('#experience-timeline').each(function () {
 
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
+		$this = $(this); // Store reference to this
+		$userContent = $this.children('div'); // user content
 
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
+		// Create each timeline block
+		$userContent.each(function () {
+			$(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+		});
 
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
+		// Add icons to each block
+		$this.find('.vtimeline-point').each(function () {
+			$(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+		});
 
-    });
+		// Add dates to the timeline if exists
+		$this.find('.vtimeline-content').each(function () {
+			var date = $(this).data('date');
+			if (date) { // Prepend if exists
+				$(this).parent().prepend('<span class="vtimeline-date">' + date + '</span>');
+			}
+		});
 
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
+	});
 
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
+	// Open mobile menu
+	$('#mobile-menu-open').click(function () {
+		$('header, body').addClass('active');
+	});
+
+	// Close mobile menu
+	$('#mobile-menu-close').click(function () {
+		$('header, body').removeClass('active');
+	});
+
+
+	// When nav bar scroll
+	var a = $("#menu").offset().top;
+
+	$(document).scroll(function () {
+		if ($(this).scrollTop() > a) {
+			$('#menu').css({
+				"background": "white",
+				"box-shadow": "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)"
+			});
+
+			// Change the links color
+			$('header a').css({
+				"color": '#3498db'
+			})
+
+		} else {
+			$('#menu').css({
+				"background": "transparent",
+				"box-shadow": "0 0 0"
+			});
+
+			// Change the links color
+			$('header a').mouseenter(function () {
+				$(this).css({
+					"color": '#3498db'
+				})
+			}).mouseleave(function () {
+				$(this).css({
+					"color": 'white'
+				})
+			})
+
+			$('header a').css({
+				"color": 'white'
+			})
+
+		}
+	});
+
+
+	$(window).resize(function () {
+		console.log($(window).width())
+
+		if ($(this).width() <= 768) {
+			// Make menu white
+			$('#menu').css({
+				"background-color": "white"
+			})
+
+			// Change the links color
+			$('header a').css({
+				"color": '#3498db'
+			})
+
+	
+			// Change the links color
+			$('header a').mouseenter(function () {
+				$(this).css({
+					"color": '#3498db'
+				})
+			}).mouseleave(function () {
+				$(this).css({
+					"color": '#3498db'
+				})
+			})
+
+
+		} else {
+			if ($(this).scrollTop() > a) {
+				$('#menu').css({
+					"background": "white",
+					"box-shadow": "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)"
+				});
+
+				// Change the links color
+				$('header a').css({
+					"color": '#3498db'
+				})
+
+			} else {
+				$('#menu').css({
+					"background": "transparent",
+					"box-shadow": "0 0 0"
+				});
+
+				// Change the links color
+				$('header a').mouseenter(function () {
+					$(this).css({
+						"color": '#3498db'
+					})
+				}).mouseleave(function () {
+					$(this).css({
+						"color": 'white'
+					})
+				})
+
+				$('header a').css({
+					"color": 'white'
+				})
+
+			}
+		}
+
+	})
+
+
+
 
 })(jQuery);
